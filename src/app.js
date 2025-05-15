fae0bffbfed1f8ccddf8dcb2ae45128e2cba38ea
Tejas030 <tejasshroff0303@gmail.com>
2025-05-15 12:54:02 +0530

    Defined Routes (CRUD)

--- src/app.js
+++ src/app.js
@@ -19,6 +19,67 @@ app.post("/signup" , async(req,res)=>{
     }
     
 })
+// This is a sample route to fetch user details
+app.get("/user" , async (req,res)=>{
+    const userDetail = req.body.email
+    try {
+        const user = await User.findOne({email : userDetail})
+        if(!user)
+        {
+            return res.status(404).send("User not found")
+        }
+        res.send(user);
+        
+    } catch (error) {
+        res.status(500).send("Error in fetching user " + error.message)
+        
+    }
+})
+// This is a sample route to fetch all users
+app.get("/feed" , async(req,res)=>{
+    try {
+        const users =  await User.find({});
+        res.send(users);
+        
+    } catch (error) {
+        res.status(500).send("Error in fetching user " + error.message)
+        
+    }
+  
+})
+
+// This is a sample route to delete user details
+app.delete("/user" , async(req,res)=>{
+    const userId = req.body.userId
+    console.log(userId)
+    try {
+        const user = await User.findByIdAndDelete(userId)
+        if(!user)
+        {
+            return res.status(404).send("User not found")
+        }
+        res.send("User deleted successfully");
+        
+    } catch (error) {
+        res.status(500).send("Error in deleting user " + error.message)
+        
+    }
+})
+
+// This  is a samp,e route to update user details
+app.patch("/user" , async(req,res)=>{
+    const userId = req.body.userId
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({_id : userId}, data)
+        res.send("Updated Succesfully");
+        
+    } catch (error) {
+        res.status(400).send("Patch Unsuccessful");
+
+
+     }
+})
 
 const port = 3000
 
@@ -33,3 +94,4 @@ connectDB().then(()=>{
     console.error("Database connection failed" + " "+ err)
 })
 
+ 

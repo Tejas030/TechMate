eca997de5e44cb0793f2dc844f674fb4bfc7fe46
Tejas030 <tejasshroff0303@gmail.com>
2025-05-16 02:08:36 +0530

    Validation using Validator

--- src/model/user.js
+++ src/model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName : {
@@ -16,11 +17,27 @@ const userSchema = new mongoose.Schema({
         unique : true,
         trim : true,
         lowercase : true,
+        validate(value)
+        {
+            if(!validator.isEmail(value))
+            {
+                throw new Error("Email is not valid")
+            }
+
+        }
     },
     password : {
         type : String,
         required : true ,
         minLength : 6,
+        validate(value)
+        {
+            if(!validator.isStrongPassword(value))
+            {
+                throw new Error("Password is not strong. It should contain at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 symbol")
+            }
+
+        }
     },
     age : {
         type : Number,
@@ -40,6 +57,13 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl : {
         type : String,
+        validate(value)
+        {
+            if(!validator.isURL(value))
+            {
+                throw new Error("Photo URL is not valid")
+            }
+        }
     },
 
     about : {
